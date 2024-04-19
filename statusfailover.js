1388aaed27487f3045bb7245605f1ac9d6a5e346
Nate Goldsborough <nathen418@playantares.com>
2024-04-18 19:16:04 -0500

    Create statusfailover.js

---
+++ statusfailover.js
@@ -0,0 +1,21 @@
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
+async function handleRequest(request) {
+  let fetchPromise = fetch(request)
+  let timeoutPromise = new Promise(resolve => setTimeout(resolve, 5000))
+  let response = await Promise.race([fetchPromise, timeoutPromise])
+
+  if (response) {
+    if ((response.status >= 500)){
+      var failoverURL = new URL(request.url)
+      failoverURL.hostname = "status2.antaresnetwork.com"
+      return fetch(failoverURL, request, { cf: { resolveOverride: 'status.antaresnetwork.com' } })
+    }
+    return response;
+    
+  } else {
+      var failoverURL = new URL(request.url)
+      failoverURL.hostname = "status2.antaresnetwork.com"
+    r  eturn fetch(failoverURL, request, { cf: { resolveOverride: 'status.antaresnetwork.com' } })
+}}
